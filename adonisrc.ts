e56f8392959d058a702856ea7980ce6c6ad94ff1
Jeremy Chaufourier <jeremy@chaufourier.fr>
2026-02-25 23:01:03 +0100

    chore: update to adonisjs 7

--- adonisrc.ts
+++ adonisrc.ts
@@ -1,3 +1,4 @@
+import { indexEntities } from '@adonisjs/core'
 import { defineConfig } from '@adonisjs/core/app'
 
 export default defineConfig({
@@ -43,7 +44,7 @@ export default defineConfig({
       file: () => import('@adonisjs/core/providers/repl_provider'),
       environment: ['repl', 'test'],
     },
-    () => import('@jrmc/adonis-mcp/mcp_provider')
+    () => import('@jrmc/adonis-mcp/mcp_provider'),
   ],
 
   /*
@@ -68,16 +69,25 @@ export default defineConfig({
   tests: {
     suites: [
       {
-        files: ['tests/unit/**/*.spec(.ts|.js)'],
+        files: ['tests/unit/**/*.spec.{ts,js}'],
         name: 'unit',
         timeout: 2000,
       },
       {
-        files: ['tests/functional/**/*.spec(.ts|.js)'],
+        files: ['tests/functional/**/*.spec.{ts,js}'],
         name: 'functional',
         timeout: 30000,
       },
+      {
+        files: ['tests/browser/**/*.spec.{ts,js}'],
+        name: 'browser',
+        timeout: 300000,
+      },
     ],
     forceExit: false,
+  },
+
+  hooks: {
+    init: [indexEntities()],
   },
 })
